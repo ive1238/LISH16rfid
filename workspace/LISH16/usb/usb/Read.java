36cb46be0b720f52dea9f62a911b1040df3a3293
ive1238 <ive1238@abv.bg>
2016-08-14 16:13:01 +0300

    return test

--- workspace/LISH16/usb/usb/Read.java
+++ workspace/LISH16/usb/usb/Read.java
@@ -1,9 +1,16 @@
 package usb;
 
 public class Read {
-	public static void main(String[] args) {
+	//public static void main(String[] args) {
 		
-arduino.read(args);
-
+//arduino.read(args);
+//	}
+	public static int asdf(){
+		int number = 532;
+        return number;
+        
 	}
+	
+
+
 }
