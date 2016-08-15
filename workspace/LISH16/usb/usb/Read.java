23b2e34561ed0c452dde259d774da1b23a1b5782
ive1238 <ive1238@abv.bg>
2016-08-15 15:41:14 +0300

    fix

--- workspace/LISH16/usb/usb/Read.java
+++ workspace/LISH16/usb/usb/Read.java
@@ -1,19 +1,12 @@
 package usb;
 
 public class Read {
-<<<<<<< HEAD
     public static int number;
 
 	public static void main(String[] args) {
-		
-arduino.read(args);
-
-=======
-	public static void main(String[] args) {
-		
-arduino.read(args);
->>>>>>> origin/master
+		arduino.read(args);
 	}
+
 	public static int asdf(){
         System.out.println(number);
 		int num = number;
